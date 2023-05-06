9eefd427762c6f84c3a292d9083d2e1f159c520e
Filip Płotnicki <fplotnicki@gmail.com>
2023-05-06 21:33:12 +0200

    nvim: remove treesitter

--- nvim_config/lua/developer/packer.lua
+++ nvim_config/lua/developer/packer.lua
@@ -19,9 +19,6 @@ return require('packer').startup(function(use)
 	  end
   })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v1.x',
